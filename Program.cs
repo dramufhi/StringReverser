9473dba6c25e8de75656605657d81715ca975281
Dakalo Ramufhi <39082407+dramufhi@users.noreply.github.com>
2023-03-27 10:39:01 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace StringReverser;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var shouldContinue = "y";
+        var manipulator = new StringManipulator();
+
+        while (shouldContinue != "n")
+        {
+            Console.WriteLine("Enter a sentense/string.\n");
+            var sentence = Console.ReadLine();
+
+            var result = !string.IsNullOrEmpty(sentence) ? manipulator.ReverseString(sentence) : "You did not type your string.";
+            Console.WriteLine(string.Concat("\n", result, "\n"));
+
+            Console.WriteLine("To continue press any key and to exit press n...");
+            shouldContinue = Console.ReadLine();
+        }
+        Console.WriteLine("\nGoodbye...");
+    }
+}
+
+class StringManipulator
+{
+    public string ReverseString(string sentence)
+    {
+        var words = sentence.Split(' ');
+        Array.Reverse(words);
+
+        return string.Join(" ", words);
+    }
+}
